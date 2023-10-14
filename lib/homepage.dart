5116c7d22613fb6543c457c1351cbdd19f59af68
Krissh Bhargava <krissh.bhargava@gmail.com>
2023-10-14 15:44:08 -0400

    new homepage

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
+import 'cart.dart'; // This line imports the Cart class from cart.dart
+
+void main() => runApp(MaterialApp(home: HomePage()));
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,9 +12,26 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  PageController _pageController = PageController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        children: [
+          HomeScreen(), 
+          BookPage(),   
+          ProfilePage(),
+          Cart(),      // This is the Cart widget you provided earlier
+        ],
+      ),
       bottomNavigationBar: Container(
         color: Colors.black,
         child: Padding(
@@ -22,31 +42,56 @@ class _HomePageState extends State<HomePage> {
             activeColor: Colors.white,
             tabBackgroundColor: Colors.grey.shade800,
             gap: 8,
-            onTabChange: (index){
-              switch (index) {
-                case 0:
-                  
-                  break;
-                default:
-              }
+            selectedIndex: _selectedIndex,
+            onTabChange: (index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+              _pageController.jumpToPage(index);
             },
             padding: EdgeInsets.all(16),
-            tabs: const[
-            GButton(
-              icon: Icons.home,
-              text: 'Home',
-            ),
-            GButton(
-              icon: Icons.book,
-              text: 'Book',
-            ),
-            GButton(
-              icon: Icons.person,
-              text: 'Profile',
-            ),
-          ]),
+            tabs: const [
+              GButton(
+                icon: Icons.home,
+                text: 'Home',
+              ),
+              GButton(
+                icon: Icons.book,
+                text: 'Book',
+              ),
+              GButton(
+                icon: Icons.person,
+                text: 'Profile',
+              ),
+              GButton(           // Add a GButton for the Cart
+                icon: Icons.shopping_cart,
+                text: 'Cart',
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Home Screen'));
+  }
+}
+
+class BookPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Book Page'));
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Profile Page'));
   }
 }
