98550b1ada2ae8d8c1396a7f052b25757df5c553
Krissh Bhargava <krissh.bhargava@gmail.com>
2023-10-14 05:59:06 -0400

    as

--- lib/main.dart
+++ lib/main.dart
@@ -3,59 +3,66 @@ import 'package:http/http.dart' as http;
 import 'package:html/parser.dart' as parser;
 import 'package:html/dom.dart' as dom;
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Web Scraper',
+      title: 'Walmart Web Scraper',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const WebScraperPage(),
+      home: WebScraperDemo(),
     );
   }
 }
 
-class WebScraperPage extends StatefulWidget {
-  const WebScraperPage({super.key});
-
+class WebScraperDemo extends StatefulWidget {
   @override
-  _WebScraperPageState createState() => _WebScraperPageState();
+  _WebScraperDemoState createState() => _WebScraperDemoState();
 }
 
-class _WebScraperPageState extends State<WebScraperPage> {
-  String? _scrapedData;
+class _WebScraperDemoState extends State<WebScraperDemo> {
+  List<String> _headings = [];
+  bool _loading = false;
 
   Future<void> _scrapeWeb() async {
+    setState(() {
+      _loading = true;
+    });
+
     final response = await http.get(Uri.parse('https://example.com'));
+
     if (response.statusCode == 200) {
       dom.Document document = parser.parse(response.body);
+      final headings = document.querySelectorAll('h1');
 
-      // Extract data from the website using selectors
-      dom.Element? titleElement = document.querySelector('title');
       setState(() {
-        _scrapedData = titleElement?.text;
+        _headings = headings.map((h1) => h1.text).toList();
       });
     }
+
+    setState(() {
+      _loading = false;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Web Scraper')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(_scrapedData ?? 'No data scraped yet'),
-            ElevatedButton(
-              onPressed: _scrapeWeb,
-              child: const Text('Scrape Data'),
-            )
-          ],
-        ),
+      appBar: AppBar(
+        title: Text('Web Scraper Demo'),
+      ),
+      body: _loading
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: _headings.length,
+              itemBuilder: (context, index) {
+                return ListTile(title: Text(_headings[index]));
+              },
+            ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _scrapeWeb,
+        child: Icon(Icons.cloud_download),
       ),
     );
   }
