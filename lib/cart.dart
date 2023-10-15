c15d74e38c188c431481e9bf89eca2148991c6dc
Krissh Bhargava <krissh.bhargava@gmail.com>
2023-10-15 00:38:25 -0400

    at gome

--- lib/cart.dart
+++ lib/cart.dart
@@ -90,7 +90,7 @@ class _CartState extends State<Cart> {
             TextField(controller: nameController, decoration: InputDecoration(labelText: 'Name')),
             TextField(controller: priceController, decoration: InputDecoration(labelText: 'Price'), keyboardType: TextInputType.number),
             CheckboxListTile(
-              title: Text("At Home")
+              title: Text("At Home"),
               value: isAtHome,
               onChanged: (newValue) {
                 setState(() {
