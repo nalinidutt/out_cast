42eb0065dae04ef11a4f76dcddbb27c20aa3265b
Nalini Dutt <72424386+nalinidutt@users.noreply.github.com>
2023-10-15 07:24:27 -0400

    Update

--- lib/home/sample_item.dart
+++ lib/home/sample_item.dart
@@ -3,6 +3,12 @@ class SampleItem {
   final String cuisine;
   final String imageURL;
   final int progress;
+  final String description;
+  final String step1;
+  final String step2;
+  final String step3;
+  final String step4;
+  final String step5;
 
-  const SampleItem(this.title, this.cuisine, this.progress, this.imageURL);
+  const SampleItem(this.title, this.cuisine, this.progress, this.imageURL, this.description, this.step1, this.step2, this.step3, this.step4, this.step5);
 }
