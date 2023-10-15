c94b15b7590b726df9b48383b29d76e9a6fa911e
Nalini Dutt <72424386+nalinidutt@users.noreply.github.com>
2023-10-15 05:32:30 -0400

    Update sample_item_details_view.dart

--- lib/home/sample_item_details_view.dart
+++ lib/home/sample_item_details_view.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:trial4/src/sample_feature/sample_item.dart';
 
 /// Displays detailed information about a SampleItem.
 class SampleItemDetailsView extends StatelessWidget {
-  const SampleItemDetailsView({super.key});
+  final SampleItem? item;
+
+  const SampleItemDetailsView({Key? key, this.item}) : super(key: key);
 
   static const routeName = '/sample_item';
 
@@ -10,10 +13,53 @@ class SampleItemDetailsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Item Details'),
+        title: Text('${item?.title ?? "Unknown"}'),
+        backgroundColor: Colors.green,
       ),
-      body: const Center(
-        child: Text('More Information Here'),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              height: 200,
+              child: Image.network(
+                item?.imageURL ?? "",
+                fit: BoxFit.cover,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    item?.title ?? "No Title",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'Cuisine: ${item?.cuisine ?? "No Cuisine"}',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'Description:',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    '${item?.description ?? "No Description"}',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
